56ded16c99629fee29a586af3f9024ec2d44f7ba
Ducky Sherwood <ducky@webfoot.com>
2014-02-24 18:31:41 -0800

    Okay, the sidebar is populated, yay!

---
+++ DomCreator.js
@@ -0,0 +1,96 @@
+// The DomCreator populates and initalizes the DOM.    It takes
+// a JSON object which describes basically everything about the
+// map.
+// The DomCreator depends, by convention, on a number of DOM 
+// elements being laid out in the view (i.e. the HTML).    These
+// include elements named
+//     explanation (p)
+//     sharingUrl (a)
+//     polygonLayers (div)
+//     dotLayers (div)
+//     borderLayers (div)
+// TODO check for existence of all the named elements
+function DomCreator ( mapApplicationInfo ) {
+    this.mai = mapApplicationInfo // shortened for typing
+}
+
+
+// Creates a selector object for a layer type (e.g. 
+// dot layers) based on how many layer specs of that type there are.
+// If there are 0, don't show a control.
+// If there are exactly 1, show a checkbox.
+// If there are >1, show a dropdown (plus a checkbox to turn them all off/on).
+// Returns a div containing a new selector control plus any explanatory text
+// @@@ Would it make more sense to create elements, then just hide
+// @@@ them if they are not used / there is not data for them?
+DomCreator.prototype.createLayerSelectorControl = function ( layerTypeName) {
+
+    if(layerTypeName in this.mai) { 
+        var layerSpecs = this.mai[layerTypeName]
+        var layerDiv = $( layerSpecs )
+        if( layerDiv == undefined ) {
+          console.log('Missing div for ' + layerspec + ', failing softly.')
+          return null
+        }
+
+        if (layerSpecs != undefined) {
+            var layerSelectionControl
+
+            // I can't just do a layerSpecs.length to get the number
+            // of layers; I can't just do a layerSpecs[0] to get
+            // the (only) element in case there is only one
+            var layersCount = 0
+            var lastKey
+            for (var key in layerSpecs) {
+                layersCount++
+                lastKey = key
+                if(layersCount > 1) break
+            }
+
+            if(layersCount == 0) {
+              return null
+            }
+
+            layerSelectionControl = document.createElement('div');
+            layerSelectionControl.id = layerTypeName + 'Control'
+
+            // Checkbox to turn all the layers on or off
+            layerSelectionCheckbox = document.createElement('input');
+            layerSelectionControl.appendChild (layerSelectionCheckbox);
+
+            layerSelectionCheckbox.checked = true
+            layerSelectionCheckbox.type = 'checkbox'
+            layerSelectionCheckbox.name = layerTypeName + 'Checkbox'
+            var layerDescriptionSpan = document.createElement('span');
+
+            if( layersCount == 1) {
+                layerSelectionCheckbox.value = lastKey
+                var longerDescription = 'Show '+layerSpecs[key].shortDescription
+                layerDescriptionSpan.textContent = longerDescription
+                layerSelectionControl.appendChild(layerDescriptionSpan)
+
+            } else {
+                
+                layerSelectionCheckbox.value = '' // don't-care
+                layerDescriptionSpan.textContent = 'Show ' + layerTypeName + ": "
+                layerSelectionControl.appendChild(layerDescriptionSpan)
+                var selectElement = document.createElement('select')
+                selectElement.className = "indented"
+                layerSelectionControl.appendChild(selectElement)
+
+                for (var key in layerSpecs) {
+                    if (layerSpecs.hasOwnProperty(key)) {
+                         var optionElement = document.createElement('option')
+                         optionElement.text = layerSpecs[key].shortDescription
+                         optionElement.setAttribute['id'] = key
+                         optionElement.value = key
+                         selectElement.appendChild(optionElement)
+                   }
+                }
+            }
+            return layerSelectionControl
+        }
+    }
+    return null
+}
+
---
+++ omni.js
@@ -0,0 +1,144 @@
+/* This file gives the information about a mapping web app, including
+ *  which polygon layers to show, 
+ *  which dot layers to show, 
+ *  whether there is cartogram information, 
+ *  what the starting lat/lng should be, 
+ *  what the colour mapping should be
+ */
+
+/* Polygon layer defaults:
+  isPercentage=false, 
+  normalizerType='n', 
+  normalizerFieldName="dont-care",
+  normalizerYear="dont-care",
+  timeSeries=constant
+  graphPrefix=don't-care; jurisdictionId appended to get the URL
+*/
+
+/* timeSeries could have { 
+  'unit' : 'month', 
+  'startingYear' : 1992,
+  'endingYear' : 2014,
+  'startingMonth' : 1,	// 1-based index
+  'endingMonth' : 5,	// 1-based index
+  }
+*/
+ 
+
+/* if missing cartogram table, then don't have a cartogram checkbox */
+mapApplicationInfo = {
+  'pageTitle' : 'Federal Spending Per Tax Dollar',
+  'pageDescription' : 'This shows the change in the tax dollars spent vs. sent by each state.  If there was $1.50 of spending in a state for each $1.00 in taxes raised from that state, it will show as -50%.',
+  'mercatorShapeType' : 'province',  // could be 'state', I suppose?
+  'mercatorPolyYear' : 2008,
+  'cartogramShapeType' : 'statePopCartogram',
+  'cartogramPolyYear' : 2011,
+  'jurisdictionInfoUrl' : '',  // e.g. ./countyPopupInformation.php
+  'startingCenterLat' : 38,
+  'startingCenterLng' : -95,
+  'startingCenterZoom' : 0,
+  'startingMarkerLat' : 0,
+  'startingMarkerLng' : 0,
+  'polygonLayers' :
+    {
+       'taxRoiNormalized': { 
+          'table':'provinceAttributes',
+          'fieldName':'taxRoiNormalized',
+          'year':2005,
+          'minValue':-1.0,
+          'maxValue':1.0,
+          'minColor' : '0000ff',
+          'maxColor' : 'ff0000',
+          'mapping' : 'plusMinus',
+          'shortDescription' : 'Tax ROI',
+          'description':'Excess/Loss Federal Spending in Each State Per Dollar of Federal Taxes',
+          'source':'Tax Foundation',
+          'sourceUrl':'http://taxfoundation.org/article/federal-spending-received-dollar-taxes-paid-state-2005'
+       },
+       'populationPovertyPct': {
+          'table':'countryLowerChamberAttributes',
+          'fieldName':'populationPovertyPct',
+          'year':2011,
+          'minValue':5,
+          'maxValue':40,
+          'minColor' : '000000',
+          'maxColor' : '0000ff',
+          'mapping' : 'linear',
+          'shortDescription' : 'Poverty Level',
+          'description':'Population earning less than the poverty line',
+          'source':'US Census Bureau',
+          'sourceUrl':'http://www2.census.gov/acs2011_1yr/CD113/EasyStats_113_Congressional_DP.xlsx'
+       },
+       'prez2012': {
+          'table':'provinceAttributes',
+          'fieldName':'demPresidentialMargin',
+          'year':2012,
+          'minValue':-0.5,
+          'maxValue':0.5,
+          'shortDescription' : 'Obama-Romney Election',
+          'description':'Democratic margin of victory in 2012 US Presidential Election',
+          'source':'Various',
+          'sourceUrl':'http://www.politico.com/2012-election/results/president/maine/'
+       },
+       'prez2008': {
+          'table':'provinceAttributes',
+          'fieldName':'obamaMargin',
+          'year':2008,
+          'minValue':-0.5,
+          'maxValue':0.5,
+          'shortDescription' : 'Obama-McCain Election',
+          'description':'Democratic margin of victory in 2008 US Presidential Election',
+          'source':'Data from many counties compiled by USA Today, M. E. J. Newman, Kaitlin Duck Sherwood, and Mark Brickly',
+          'sourceUrl':'http://www.usatoday.com/news/politics/election2008/president.htm'
+       },
+    },
+  'borderLayers' :
+    {
+       'county': {
+          'table':'countyAttributes',
+          'mercatorType' : 'state',
+          'cartogramType' : 'statePopCartogram',
+          'fieldName':'jurisdictionId',
+          'year':2000,
+          'minValue':0,
+          'maxValue':4000,
+          'minColor' : 'ffffff',
+          'maxColor' : '000000',
+          'mapping' : 'none',
+          'shortDescription' : 'County',
+          'description' : 'County borders',
+          'source':'US Census Bureau',
+          'sourceUrl':'http://www2.census.gov/acs2011_1yr/CD113/EasyStats_113_Congressional_DP.xlsx'
+       },
+       'state': {
+          'table':'provinceAttributes',
+          'fieldName':'jurisdictionId',
+          'mercatorType' : 'county',
+          'cartogramType' : 'countyPopulationCartogram',
+          'year':2000,
+          'minValue':0,
+          'maxValue':70,
+          'shortDescription' : 'State',
+          'description' : 'State borders',
+          'source':'US Census Bureau',
+          'sourceUrl':'http://www2.census.gov/acs2011_1yr/CD113/EasyStats_113_Congressional_DP.xlsx'
+       },
+    },
+  'dotLayers' :
+    {
+       'shutdownSigners': {
+          'mercatorTable':'dotAttributes',
+          'mercatorFieldName':'shutdownSigner',
+          'cartogramTable':'congressionalDistrictPopCartDotAttributes',
+          'cartogramFieldName':'shutdownSignerCart',
+          'year':2013,
+          'size':3,
+          'color': '000005',
+          'shortDescription' : "dots for representatives who supported the 2014 US Federal shutdown",
+          'description': 'Republican Congressional Representatives who signed a letter supporting shutting down the US Federal government in an attempt to defund Obamacare.',
+          'source' : 'http://www.freedomworks.org/content/have-your-members-congress-signed-defund-obamacare-letter-find-out-here'
+       }
+    }
+	
+
+}
