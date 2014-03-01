ccd51b9bab878464ed3901bb774654cc7ff33882
Ducky Sherwood <ducky@webfoot.com>
2014-03-01 14:31:14 -0800

    Can now click on marker to open infowindow

--- ListenerInitializer.js
+++ ListenerInitializer.js
@@ -3,7 +3,7 @@ function ListenerInitializer (map, mapApplicationInfo,
   this.map = map
   this.mai = mapApplicationInfo
   this.cityLabeller = labeller
-  this.marker = jurisdictionMarker
+  this.jurisdictionMarker = jurisdictionMarker
 
   this.addLayerControlSelectListener('choroplethLayers') 
   this.addLayerControlSelectListener('borderLayers') 
@@ -12,15 +12,25 @@ function ListenerInitializer (map, mapApplicationInfo,
   this.addLayerControlCheckboxListener('dotLayers')
   this.addCitiesCheckboxListener()
   this.addIsCartogramCheckboxListener()
-  this.addClickListener()
+  this.addMapClickListener()
+  this.addMarkerClickListener()
+
 
   // There isn't an obvious place to put this
   this.updateSharingUrl()
   
 }
 
+
 ListenerInitializer.prototype.updateSharingUrl = function() {
   $( '#sharingUrl' )[0].href = this.map.getSharingUrl()
+}
+
+ListenerInitializer.prototype.addMarkerClickListener = function() {
+  var scope = this
+  this.jurisdictionMarker.on("click", function (e) {
+    scope.requestPopupInformation(e)
+  })
 }
 
 ListenerInitializer.prototype.addLayerControlSelectListener
@@ -88,58 +98,73 @@ ListenerInitializer.prototype.addIsCartogramCheckboxListener
     closureCityLabeller.refreshCityLabels(closureCityLabeller)    
     scope.updateSharingUrl()
   }
-
-ListenerInitializer.prototype.addClickListener = function() {
-  
-    var closureMap = this.map
-    var closureJurisdictionMarker = this.marker
-
-    var scope = this
-    closureMap.on("click", function (e) {
-  
-      var latlng = e.latlng
-      var lat = latlng.lat
-      var lng = latlng.lng
-  
-      var setPopupInfo = function (responseText) {
-        closureJurisdictionMarker.setPopupContent(responseText);
-        scope.updateSharingUrl()
-      }
-
-      if(closureJurisdictionMarker) {
-        closureJurisdictionMarker.setLatLng([lat, lng]);
-        // @@@ TODO set flag in omni.js to say iff popup should open always
-        closureJurisdictionMarker.openPopup();
-      }
-    
-      var isCartogramCheckbox = document.getElementById("isCartogramCheckbox");
-      var cartogramFlag;
-    
-      
-      var layerName = closureMap.getLayerName('dotLayers')
-      var fieldName, year
-      if(layerName) {
-        fieldName = closureMai['dotLayers'][layerName].fieldName
-        year = closureMai['dotLayers'][layerName].year
-      } else {
-        fieldName = null
-        year =null
-      }
-      var cartogramFlag = closureMap.getFlagForCheckbox('#isCartogramCheckbox')
-    
-      var url = closureMap.pointInfoUrlPrefix + "?" +
-         "lat="+lat+"&lng="+lng+"&zoom="+map.zoom
-         +"&fieldName="+fieldName 
-         + "&polyYear=2011&year=2011&cartogram="+cartogramFlag;
-
-      closureJurisdictionMarker.setPopupContent("Looking up congressional district information, please wait...");
-
-
-      requestUrlWithScope(url, setPopupInfo, this);  // request is a verb here
-    })
-  }
-  
 }
+
+ListenerInitializer.prototype.setPopupInfoCallback = function (responseText) {
+  this.jurisdictionMarker.setPopupContent(responseText);
+  this.updateSharingUrl()
+}
+
+ListenerInitializer.prototype.requestPopupInformation = function (e) {
+    var latlng = e.latlng
+    var lat = latlng.lat
+    var lng = latlng.lng
+
+    // TODO this piece needs to be split out separately so that
+    // I can use it also for clicking on the marker (see addMarkerClickListener,
+    // above
+    var isCartogramCheckbox = document.getElementById("isCartogramCheckbox");
+    var cartogramFlag;
+
+    var layerName = this.map.getLayerName('dotLayers')
+    var fieldName, year
+    if(layerName) {
+      fieldName = this.mai['dotLayers'][layerName].fieldName
+      year = this.mai['dotLayers'][layerName].year
+    } else {
+      fieldName = null
+      year =null
+    }
+    var cartogramFlag = this.map.getFlagForCheckbox('#isCartogramCheckbox')
+  
+
+    var url = this.map.pointInfoUrlPrefix + "?" 
+       + "lat="+this.map.getCenter().lat+"&lng="+this.map.getCenter().lng
+       +"&zoom="+this.map.zoom
+       +"&fieldName="+fieldName 
+       + "&polyYear=2011&year=2011&cartogram="+cartogramFlag;
+
+    this.jurisdictionMarker.setPopupContent("Looking up congressional district information, please wait...");
+
+    requestUrlWithScope(url, this.setPopupInfoCallback, this);  // request is a verb here
+    }
+
+ListenerInitializer.prototype.addMapClickListener = function() {
+
+  var closureMap = this.map
+  var closureJurisdictionMarker = this.jurisdictionMarker
+
+  var scope = this
+  closureMap.on("click", function (e) {
+
+    var latlng = e.latlng
+    var lat = latlng.lat
+    var lng = latlng.lng
+
+
+    if(closureJurisdictionMarker) {
+      closureJurisdictionMarker.setLatLng([lat, lng]);
+      // @@@ TODO set flag in omni.js to say iff popup should open always
+      closureJurisdictionMarker.openPopup();
+    }
+  
+    scope.requestPopupInformation(e)    
+
+  })
+
+}
+  
+
 
 
 
