c78b65f8695d9846692e7005e9521795ad6cfd01
Ducky Sherwood <ducky@webfoot.com>
2014-03-04 00:49:44 -0800

    Added JSDoc and semicolons.

--- CityLabeller.js
+++ CityLabeller.js
@@ -1,8 +1,30 @@
-function CityLabeller(aMapDiv, aMap) {
-  this.mapDiv = aMapDiv;
+/** @author Kaitlin Duck Sherwood
+ *  @class CityLabeller
+ *  @classdesc This class shows cities on leaflet map at the appropriate
+ *    place, which is tricky because the map can be mercator or 
+ *    cartogram.  Instead of trying to take the time to calculate the 
+ *    lat/lng for the cartogram on the fly from the mercator lat/lng,
+ *    it's precalculated in a different database table or under a different
+ *    field name; the table/fieldName is specified in the mapApplicationInfo.
+ * 
+ *  @constructor
+ *  @this {ListenerInitializer}
+ *  @param aMap {object} A Leaflet map object.
+ *  @param aJurisdictionMarker {object} A Leaflet marker
+ */
+function CityLabeller(aMap) {
   this.mapObject = aMap;
-  this.cityMarkers = []
+  this.cityMarkers = [];
 
+  /** Fetches the largest cities in a bounding box, with their name and lat/lng.
+   *  @param anUpper {float} The northernmost boundary of the map's view
+   *  @param aLower {float} The southernmost boundary of the maps's view
+   *  @param aLeft {float} The westernmost boundary of the map's view
+   *  @param aRight {float} The easternmost boundary of the map's view
+   *  @param isCartogram {boolean} Whether the map project is a cartogram
+   *  @private
+   * SIDE EFFECT: Sends off an HTTP request
+   */
   this.requestCityInfo = function (anUpper, aLower, aLeft, aRight, 
                                    isCartogram) {
     // Because this is python, it needs to be in cgi-bin.  PHP is the only
@@ -13,12 +35,16 @@ function CityLabeller(aMapDiv, aMap) {
 
     // request is a verb here
     requestUrlWithScope(url, this.showCityLabels, this);  
-  }
+  };
   
   
+  /** Removes all the city labels from the map.
+   *  @private
+   *  SIDE EFFECT: removes all the city labels from the map
+   */
   this.removeCityLabels = function() {
-    // var mapDiv = $( '#map')[0]
-    var kids = this.mapDiv.childNodes
+    var mapDiv = $( '#map')[0]
+    var kids = mapDiv.childNodes
     var klen = kids.length;
   
     // need to remove labels starting at the back of the list so that
@@ -42,6 +68,12 @@ function CityLabeller(aMapDiv, aMap) {
     }
   }
   
+  /** Fetches the largest cities in a bounding box, with their name and lat/lng.
+   *  @param responseText {object} A JSON string with information about 
+   *    the visible cities 
+   *  @private
+   * SIDE EFFECT: shows cities' names on the map
+   */
   this.showCityLabels = function(responseText) {
     // http://stackoverflow.com/questions/9912145/leaflet-how-to-find-existing-markers-and-delete-markers
     var cities = JSON.parse(responseText);
@@ -74,6 +106,12 @@ function CityLabeller(aMapDiv, aMap) {
     
   }
   
+  /** Shows the largest cities on the map.
+   *  @param scope {object} The scope which should be used for removing
+   *    city labels
+   *  @private
+   * SIDE EFFECT: shows cities' names on the map
+   */
   this.refreshCityLabels = function(scope) {
   
     var bounds = this.mapObject.getBounds();
