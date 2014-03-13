5a1acbce60b962a2e1c9e9e8690c4200f83a9e12
Ducky Sherwood <ducky@webfoot.com>
2014-03-12 17:35:48 -0700

    Comment.

--- integrationTest/incompleteMai3.js
+++ integrationTest/incompleteMai3.js
@@ -1,4 +1,3 @@
-/* if missing cartogram table, then don't have a cartogram checkbox */
 mapApplicationInfo = {
   'pageTitle' : 'TestSanity pageTitle',
   'pageDescription' : 'TestSanity pageDescription',
