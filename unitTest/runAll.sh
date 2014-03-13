7722d37a6f3efdc37346f74f4318a8b63bdfccdc
Ducky Sherwood <ducky@webfoot.com>
2014-03-13 16:38:09 -0700

    New

---
+++ unitTest/runAll.sh
@@ -0,0 +1,6 @@
+#/bin/sh
+export CLASSPATH=/appdata/bin/springsource/sts-3.0.0.RELEASE/plugins/org.mozilla.javascript_1.7.2.v201005080400.jar
+# java org.mozilla.javascript.tools.debugger.Main -f ./AllTests.js
+java org.mozilla.javascript.tools.shell.Main -f ./AllTests.js
+
+
